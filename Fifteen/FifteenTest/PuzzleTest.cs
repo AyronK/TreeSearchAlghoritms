6ab79b5776aadeba26f263fe90798e7ff2dccb50
AyronKotlet <AyronKotlet@users.noreply.github.com>
2017-10-07 14:54:53 +0200

    Puzzle init and test

---
+++ Fifteen/FifteenTest/PuzzleTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PuzzleSolver;
+using System.Linq;
+
+namespace FifteenTest
+{
+    [TestClass]
+    public class PuzzleTest
+    {
+        [TestMethod]
+        public void PuzzleInitTest()
+        {
+            int dimentionLenght = 3;
+            Puzzle puzzle = new Puzzle(dimentionLenght);
+            int sum = 0;
+            foreach (var cell in puzzle.ToArray())
+            {
+                sum += cell;
+            }
+            int firstCell = 0;
+            int count = (int)Math.Pow(dimentionLenght, 2);
+            int lastCell = count - 1;
+
+            Assert.AreEqual(firstCell + lastCell / 2.0 * count, sum);
+        }
+    }
+}
---
+++ Fifteen/PuzzleSolver/Puzzle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleSolver
+{
+    public class Puzzle
+    {
+        private static Random random = new Random();
+
+        private int[,] puzzle;
+
+        public Puzzle(int dimentionLenght)
+        {
+            puzzle = new int[dimentionLenght, dimentionLenght];
+
+            int[] values = new int[dimentionLenght * dimentionLenght];
+            for (int i = 0; i < values.Length; i++)
+            {
+                values[i] = i;
+            }
+            values = values.OrderBy(x => random.Next(0, dimentionLenght)).ToArray();
+
+            for (int i = 0; i < values.Length; i++)
+                puzzle[i / dimentionLenght, i % dimentionLenght] = values[i];
+        }
+
+        public int GetValue(int row, int column) => puzzle[row, column];
+
+        public int this[int row, int column] => GetValue(row, column);
+
+        public int[,] ToArray()
+        {
+            return puzzle;
+        }
+    }
+}
